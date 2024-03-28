b2bde7738e89edd01140982d31be8d22c61b5a9d
nimrod njau <nimrodnjau@gmail.com>
2024-03-28 21:31:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+let namesArray = ["Tracy", "George", "Peter"]
+let event = "surprise"
+
+function writeCards (namesArray, event) {
+    let cardMessage = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      cardMessage.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardMessage
+  }
+    console.log(writeCards(namesArray, event))
+
+
+
+function countDown() {
+    let startingNumber = 10
+    while (startingNumber <= 10 && startingNumber > 0) {
+        console.log(startingNumber)
+        startingNumber -= 1
+    }
+    console.log(startingNumber)
+}
+
+
+
+
+
